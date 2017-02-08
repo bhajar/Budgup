fe81a0946665adb6cf43d8b659f67f011ebcef29
KhaoulaZitoun <khaoula.zitoun@gmail.com>
2017-02-08 16:29:33 +0100

    Gulp file for build

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+
+
+
+gulp.task('default', function() {
+    console.log("La task default a bien été lancée")
+});
+
+gulp.task('build', function() {
+    console.log("La task build a bien été envoyée")
+});
