f1e6e62d3a7d56adca02fd8a16210f1e4473c055
bhajar <projetbudgup@gmail.com>
2017-03-19 15:07:57 +0100

    improved upload and the algo tree versions test

--- test/index.js
+++ test/index.js
@@ -1,41 +1,76 @@
 var assert = require('assert');
 var request = require('request');
 var expect = require('chai').expect;
+var fs = require('fs');
 
 var port = 9250;
 var url = "http://127.0.0.1:"+port;
 
-function get_url(url) {
+function upload() {
     return new Promise(function(resolve, reject) {
-        request(url, function(error, response, body) {
-            if (error) {
-                resolve("Impossible de charger l'URL");
+        var formData = {
+            file: fs.createReadStream(__dirname + '/data.json')
+        };
+        request.post({ url: url+'/upload', formData: formData}, function(err, httpResponse, body) {
+            if (err) {
+                resolve("Impossible d'uploader le fichier json.");
             }
-
-            resolve('Opérationel');
+            resolve("Opérationel");
         });
     });
 }
 
-describe('Teste des Algos', function() {
+function check_operation(url, raw) {
+    return new Promise(function(resolve, reject) {
+        request(url, function(error, response, body) {
+            if (error) {
+                resolve(false);
+            }
+
+            try {
+                var operation_found = false;
+                var operations = JSON.parse(body);
+                for (var i = 0; i < operations.length; i++) {
+                    if (operations[i].raw == raw) {
+                        operation_found = true;
+                        break;
+                    }
+                }
+                resolve(operation_found);
+
+            } catch(err) {
+                resolve(false);
+            }
+        });
+    });
+}
+
+describe('Upload', function() {
+    it('POST /upload', function() {
+        return upload().then(function(result) {
+            expect(result).to.equal('Opérationel');
+        });
+    });
+});
+describe('Test des Algos', function() {
     describe('Algo de base', function() {
         it('GET /get_operations1', function() {
-            return get_url(url+"/get_operations1").then(function(result) {
-                expect(result).to.equal('Opérationel');
+            return check_operation(url+"/get_operations1", "operation 10").then(function(result) {
+                expect(result).to.equal(true);
             });
         });
     });
     describe('Already Paid', function() {
         it('GET /get_operations2', function() {
-            return get_url(url+"/get_operations2").then(function(result) {
-                expect(result).to.equal('Opérationel');
+            return check_operation(url+"/get_operations2", "EDF").then(function(result) {
+                expect(result).to.equal(true);
             });
         });
     });
     describe('Min Max', function() {
         it('GET /get_operations', function() {
-            return get_url(url+"/get_operations").then(function(result) {
-                expect(result).to.equal('Opérationel');
+            return check_operation(url+"/get_operations", "EDF").then(function(result) {
+                expect(result).to.equal(true);
             });
         });
     });
